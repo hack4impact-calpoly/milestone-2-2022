04c45bcb290f1d690fe3b0f0af33cf73b2dab32a
javalosr2004 <jesusavalosr2004@gmail.com>
2022-10-28 09:53:21 -0700

    feat: milestone-2

--- milestone2.ts
+++ milestone2.ts
@@ -1,5 +1,7 @@
 // This is the data we'll be working with!
 
+import { volunteers } from "./tests/data";
+
 interface Volunteer {
   name: string;
   age: number;
@@ -83,34 +85,49 @@ const userData: Volunteer[] = [
   Hint: user data is stored in the userData object above. 
 */
 type GetNumber = (data: Volunteer[]) => number;
-let findAverage: GetNumber; // Define lambda function here
+let findAverage: GetNumber = (data) => {
+  let total_age: number = 0;
+  for (const volunteer of data){
+    total_age += volunteer.age;
+  }
+  return total_age / data.length
 
-//console.log(findAverage(userData));
+}; // Define lambda function here
+
+
+console.log(findAverage(userData));
 
 // Question 2: Data Handling
 /* Use data handling function(s) to find the first index of someone from San Francisco (SF).
   Return -1 if no one is from San Francisco.
   Hint: Use a lambda function as a value
 */
-let findIndexAns: GetNumber; // Code here
+let findIndexAns: GetNumber = (data) => {
+  const findSF = (volunteer:Volunteer) => volunteer.city === 'SF';
+  return data.findIndex(findSF)
+}; // Code here
 
-//console.log(findIndexAns(userData));
+console.log(findIndexAns(userData));
 
 // Question 3: Filtering data
 /* Use data handling function(s) to find all of the volunteers from California (CA) over an age threshold n
  */
 type GetVolunteers = (data: Volunteer[], minAge: number) => Volunteer[];
-let findCAOverN: GetVolunteers; // Code here
+let findCAOverN: GetVolunteers = (data, minAge) => {
+  return data.filter(volunteer => volunteer.state === 'CA' && volunteer.age >= minAge)
+} // Code here
 
-//console.log(findCAOverN(userData, 25));
+console.log((findCAOverN(userData, 25)));
 
 // Question 4: Searching Data
 /* Use data handling function(s) to find the first staff member from Santa Barbara (SB)
  */
 type GetVolunteer = (data: Volunteer[]) => Volunteer | undefined;
-let findSBStaff: GetVolunteer; // Code here
+let findSBStaff: GetVolunteer = (data) => {
+  return data.find(volunteer => volunteer.city == 'SB')
+}; // Code here
 
-//console.log(findSBStaff(userData));
+console.log(findSBStaff(userData));
 
 // Question 5: Spread Operator Part 1
 let kyle: Volunteer = {
@@ -125,10 +142,12 @@ let kyle: Volunteer = {
 /* Lets make a clone of Kyle above using the spread operator and assign it to kyleClone
  */
 type CopyVolunteer = (vol: Volunteer) => Volunteer;
-let copyVolunteer: CopyVolunteer; // Code here
+let copyVolunteer: CopyVolunteer = (vol) => {
+  return {...vol}
+}// Code here
 
-// let kyleClone: Volunteer = copyVolunteer(kyle);
-//console.log(kyleClone);
+let kyleClone: Volunteer = copyVolunteer(kyle);
+console.log(kyleClone);
 
 // Question 6: Spread Operator Part 2
 /* Next, lets use the spread operator to update your kyleClone object with the updatedLocation defined below and assign it to kyleNew
@@ -139,10 +158,12 @@ type UpdateVolunteer = (
   vol: Volunteer,
   updates: Partial<Volunteer>
 ) => Volunteer;
-let updateVolunteer: UpdateVolunteer; // Code here
+let updateVolunteer: UpdateVolunteer = (vol, updates) => {
+  return {...vol, ...updates}
+} // Code here
 
-// let kyleNew = updateVolunteer(kyleClone);
-//console.log(kyleNew);
+kyleNew = updateVolunteer(kyleClone, updatedLocation);
+console.log(kyleNew);
 
 // Question 7: Object Destructuring
 /* Now that we have our updated kyle, lets use object destructuring to get his name, age, and city
@@ -150,12 +171,12 @@ let updateVolunteer: UpdateVolunteer; // Code here
 
 type GetVolunteerInfo = (vol: Volunteer) => String;
 let getVolunteerInfo: GetVolunteerInfo = (vol) => {
-  let name, age, city; // Code here
+  let {name: name, age: age, city: city} = vol; // Code here
   return `${name} is ${age} years old and lives in ${city}`;
 };
 
-// let kyleInfo = getVolunteerInfo(kyleNew);
-//console.log(kyleInfo);
+let kyleInfo = getVolunteerInfo(kyleNew);
+console.log(kyleInfo);
 
 // Question 8: Putting it All Together!
 /* Use all the skills we've covered today to get the first user from 
@@ -164,9 +185,16 @@ let getVolunteerInfo: GetVolunteerInfo = (vol) => {
 /* these criteria, return undefined
  */
 
-let daBigTest: GetVolunteer; // Code here
+let daBigTest: GetVolunteer = (data) => {
+  const slo_data: Volunteer | undefined = data.find(volunteer => (volunteer.city === 'SLO' && volunteer.age > 40) ? volunteer : undefined);
+  if (slo_data){
+    return {...slo_data, position:'staff'}
+  }
+  return slo_data
 
-//console.log(daBigTest(userData));
+}; // Code here
+
+console.log(daBigTest(userData));
 
 export {
   Volunteer,
