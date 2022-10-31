c019f4d6e1fb95cc9872107bb035940f9420a713
upluke <lukeforadream@gmail.com>
2022-10-30 23:46:31 -0700

    final

--- milestone2.ts
+++ milestone2.ts
@@ -82,7 +82,7 @@ const userData: Volunteer[] = [
   Hint: user data is stored in the userData object above. 
 */
 type GetNumber = (data: Volunteer[]) => number;
-let findAverage:GetNumber=(userData)=>userData.reduce((acc, cur)=>acc+cur.age, 0) / userData.length; // Define lambda function here
+let findAverage:GetNumber=(data)=>data.reduce((acc, cur)=>acc+cur.age, 0) / data.length; // Define lambda function here
  
 // console.log(findAverage)
 console.log(findAverage(userData));
@@ -92,25 +92,25 @@ console.log(findAverage(userData));
   Return -1 if no one is from San Francisco.
   Hint: Use a lambda function as a value
 */
-let findIndexAns: GetNumber; // Code here
+let findIndexAns: GetNumber =(data)=>data.findIndex(d=>d.city==='SF'); // Code here
 
-//console.log(findIndexAns(userData));
+console.log(findIndexAns(userData));
 
 // Question 3: Filtering data
 /* Use data handling function(s) to find all of the volunteers from California (CA) over an age threshold n
  */
 type GetVolunteers = (data: Volunteer[], minAge: number) => Volunteer[];
-let findCAOverN: GetVolunteers; // Code here
+let findCAOverN: GetVolunteers=(data, minAge)=>data.filter(d=>d.age>minAge && d.state=='CA' ); // Code here
 
-//console.log(findCAOverN(userData, 25));
+console.log(findCAOverN(userData, 25));
 
 // Question 4: Searching Data
 /* Use data handling function(s) to find the first staff member from Santa Barbara (SB)
  */
 type GetVolunteer = (data: Volunteer[]) => Volunteer | undefined;
-let findSBStaff: GetVolunteer; // Code here
+let findSBStaff: GetVolunteer=data=>data.find(d=>d.city==='SB'); // Code here
 
-//console.log(findSBStaff(userData));
+console.log(findSBStaff(userData));
 
 // Question 5: Spread Operator Part 1
 let kyle: Volunteer = {
@@ -125,24 +125,28 @@ let kyle: Volunteer = {
 /* Lets make a clone of Kyle above using the spread operator and assign it to kyleClone
  */
 type CopyVolunteer = (vol: Volunteer) => Volunteer;
-let copyVolunteer: CopyVolunteer; // Code here
+let copyVolunteer: CopyVolunteer=vol=>({...vol}); // Code here
 
-// let kyleClone: Volunteer = copyVolunteer(kyle);
-//console.log(kyleClone);
+let kyleClone: Volunteer = copyVolunteer(kyle);
+console.log(kyleClone);
 
 // Question 6: Spread Operator Part 2
 /* Next, lets use the spread operator to update your kyleClone object with the updatedLocation defined below and assign it to kyleNew
  */
 let updatedLocation = { city: "Seattle", state: "WA" };
-let kyleNew: Volunteer;
+ 
+
 type UpdateVolunteer = (
   vol: Volunteer,
   updates: Partial<Volunteer>
 ) => Volunteer;
-let updateVolunteer: UpdateVolunteer; // Code here
 
-// let kyleNew = updateVolunteer(kyleClone);
-//console.log(kyleNew);
+let updateVolunteer: UpdateVolunteer = (data,newData) => {
+  return {...data, ...newData};
+};// Code here
+
+let kyleNew = updateVolunteer(kyleClone, updatedLocation);
+// console.log(kyleNew);
 
 // Question 7: Object Destructuring
 /* Now that we have our updated kyle, lets use object destructuring to get his name, age, and city
@@ -150,12 +154,13 @@ let updateVolunteer: UpdateVolunteer; // Code here
 
 type GetVolunteerInfo = (vol: Volunteer) => String;
 let getVolunteerInfo: GetVolunteerInfo = (vol) => {
-  let name, age, city; // Code here
+  let {name, age, city}=vol; // Code here
+
   return `${name} is ${age} years old and lives in ${city}`;
 };
 
-// let kyleInfo = getVolunteerInfo(kyleNew);
-//console.log(kyleInfo);
+let kyleInfo = getVolunteerInfo(kyleNew);
+console.log(kyleInfo);
 
 // Question 8: Putting it All Together!
 /* Use all the skills we've covered today to get the first user from 
@@ -164,9 +169,13 @@ let getVolunteerInfo: GetVolunteerInfo = (vol) => {
 /* these criteria, return undefined
  */
 
-let daBigTest: GetVolunteer; // Code here
+let daBigTest: GetVolunteer=vol=>{
+  const p=vol.find(v=>v.city==='SLO' && v.age>40)
+  if(p) p.position='staff'
+  return p
+}; // Code here
 
-//console.log(daBigTest(userData));
+console.log(daBigTest(userData));
 
 export {
   Volunteer,
